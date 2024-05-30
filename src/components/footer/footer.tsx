fa3c7b058df4d78e6e9c228e1307c9d80198848c
Morbius00 <rajsaha20026@gmail.com>
2024-05-30 22:49:45 +0530

    Launch: Vikray_Update2.0

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "../common/button";
+
+const footer = () => {
+  return (
+    <footer className=" bg-black ">
+      <div className="flex md:flex-row flex-col md:space-x-[4vw] py-16">
+        <div className="lg:mt-16 lg:text-center text-5xl font-bold  lg:ml-16 ml-7 ">
+          A step <span className="text-[#FFFF6D]">forward</span> towards{" "}
+          <span className="text-[#FFFF6D]">web3</span>
+        </div>
+        <div>
+          <div className="text-lg font-semibold ml-7 mt-3 ">
+            Phone <br /> +91 9123755164
+          </div>
+          <div className="text-lg font-semibold ml-7 mt-3">
+            Email <br /> vikray.web3@gmail.com
+          </div>
+          <div>
+            <div className="text-lg font-semibold ml-7 mt-3 ">Socials</div>
+          </div>
+        </div>
+
+        <div className="bg-[#FFFF6D] md:w-2 md:h-24 rounded-lg"></div>
+        <div>
+          <div className="text-xl font-bold text-white pb-9 ml-7 mt-3">
+            Excited for your new shopping journey?{" "}
+          </div>
+          <Button filled className="text-lg ml-7">
+            Let Shop
+          </Button>
+        </div>
+      </div>
+      <div className="text-center py-5 text-xl">Made By Morbius</div>
+    </footer>
+  );
+};
+export default footer;
---
+++ src/pages/index.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import Button from '@/components/common/button'
+import Navbar from '@/components/navbar/navbar'
+import React from 'react'
+import Image from "next/image";
+import {HeroImage} from '../assets/landingAssets/index'
+import HomeComp from '../components/Landing component/landingcomponent'
+import Footer from '@/components/footer/footer'
+
+
+
+const landing = () => {
+  return (
+    <div className="bg-[url('/bg-image.png')] bg-[#212121] text-white h-full overflow-hidden ">
+      <Navbar />
+      <div className="flex md:space-x-20 md:flex-row flex-col  lg:mx-28 lg:my-20 mx-7 my-5">
+        <div>
+          <div className="lg:text-[2.2vw] md:text-[29px] text-[5vw] ">
+            Introducing the
+            <span className="text-[#FFFF6D]"> future of fashion</span> with our
+            web3 clothing store e-commerce
+          </div>
+          <div className="md:flex md:flex-col">
+            <div className="mt-6 lg:mt-14 lg:text-[1.1vw] md:text-[1.2vw] ">
+              Join the revolution of sustainable fashion today.Check out our
+              latest arrivals and stay up-to-date with the newest trends in
+              sustainable fashion. Sign up for our newsletter to receive
+              exclusive updates, promotions and sustainable fashion tips.
+              Experience the future of fashion today! Shop now and join the
+              revolution of sustainable fashion.
+            </div>
+            <div className=" flex flex-row  space-x-9 my-12">
+              <Button filled className="text-base">
+                Explore Shop
+              </Button>
+              <Button className="text-white text-lg border-2 border-[#FFFF6D]">
+                Know more
+              </Button>
+            </div>
+          </div>
+        </div>
+       
+        <div className="flex flex-col mt-5 ">
+          <div className="lg:w-[50vw] md:w-[40vw] flex flex-col lg:flex-row w-auto">
+            <Image src={HeroImage} alt="Picture of the author" />
+          </div>
+          <div className="mt-6 text-[7vw] lg:text-[3vw] md:text-[2vw] ml-9 text-[#FFFF6D]">
+            web3 * Fashion * AR
+          </div>
+        </div>
+      </div>
+      
+      <div className='mt-16'>
+        <div className="bg-[#FFFF6D] h-1 w-full "></div>
+        <div className="text-center text-[7vw] font-bold text-[#FFFF6D] md:justify-evenly">
+          METAVERSE SHOPPING
+        </div>
+        <div className="bg-[#FFFF6D] h-1 w-full"></div>
+      </div>
+      <HomeComp/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default landing
