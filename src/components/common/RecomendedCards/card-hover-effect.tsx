ebffa6b2548167a09b226f2cd679cd05887bc04d
Morbius00 <rajsaha20026@gmail.com>
2024-07-14 19:14:31 +0530

    Built: Trending pics section in the product page

--- src/components/common/RecomendedCards/card-hover-effect.tsx
+++ src/components/common/RecomendedCards/card-hover-effect.tsx
@@ -38,14 +38,14 @@ export const HoverEffect = ({
           <Link
             href={item?.link}
             key={item?.link}
-            className="relative group  block p-2 h-full w-full"
+            className="relative group block p-2 h-full w-full"
             onMouseEnter={() => setHoveredIndex(idx)}
             onMouseLeave={() => setHoveredIndex(null)}
           >
             <AnimatePresence>
               {hoveredIndex === idx && (
                 <motion.span
-                  className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-[#FFFF6D]/[0.98] block  rounded-3xl"
+                  className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-[#FFFF6D]/[0.98] block rounded-3xl"
                   layoutId="hoverBackground"
                   initial={{ opacity: 0 }}
                   animate={{
@@ -62,7 +62,7 @@ export const HoverEffect = ({
             <Card>
               {/* <CardTitle>{item.title}</CardTitle>
             <CardDescription>{item.description}</CardDescription> */}
-              <CardImage className=" mx-auto  mt-4">
+              <CardImage className=" mx-auto">
                 <Image
                   src={item.image}
                   width={2000}
